0b2ccc7847d4135535bd87d4d7687734dbf7fcc5
Michael Tseng <sabonis.tw@gmail.com>
2012-12-20 12:24:36 +0800

    set weight properties on tabgroup class

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/tabgroup/TiUITabHostGroup.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/tabgroup/TiUITabHostGroup.java
@@ -42,159 +42,160 @@ import android.widget.TabWidget;
  * TabHost starts when that window's tab is selected.
  */
 public class TiUITabHostGroup extends TiUIAbstractTabGroup
-		implements OnTabChangeListener, TabContentFactory {
+    implements OnTabChangeListener, TabContentFactory {
 
-	private static final String TAG = "TiUITabHostGroup";
+    private static final String TAG = "TiUITabHostGroup";
 
-	private TabHost tabHost;
-	private final HashMap<String, TiUITabHostTab> tabViews = new HashMap<String, TiUITabHostTab>();
+    private TabHost tabHost;
+    private final HashMap<String, TiUITabHostTab> tabViews = new HashMap<String, TiUITabHostTab>();
 
-	public TiUITabHostGroup(TabGroupProxy proxy, TiBaseActivity activity)
-	{
-		super(proxy, activity);
-		setupTabHost();
+    public TiUITabHostGroup(TabGroupProxy proxy, TiBaseActivity activity)
+    {
+        super(proxy, activity);
+        setupTabHost();
 
-		TiCompositeLayout.LayoutParams params = new TiCompositeLayout.LayoutParams();
-		params.autoFillsHeight = true;
-		params.autoFillsWidth = true;
+        TiCompositeLayout.LayoutParams params = new TiCompositeLayout.LayoutParams();
+        params.autoFillsHeight = true;
+        params.autoFillsWidth = true;
 
-		((TiCompositeLayout) activity.getLayout()).addView(tabHost, params);
-	}
+        ((TiCompositeLayout) activity.getLayout()).addView(tabHost, params);
+    }
 
-	private void setupTabHost() {
-		Context context = TiApplication.getInstance().getApplicationContext();
-		LayoutParams params;
+    private void setupTabHost() {
+        Context context = TiApplication.getInstance().getApplicationContext();
+        LayoutParams params;
 
-		tabHost = new TabHost(context, null);
-		tabHost.setId(android.R.id.tabhost);
-		params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-		tabHost.setLayoutParams(params);
+        tabHost = new TabHost(context, null);
+        tabHost.setId(android.R.id.tabhost);
+        params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        tabHost.setLayoutParams(params);
 
-		LinearLayout container = new LinearLayout(context);
-		container.setOrientation(LinearLayout.VERTICAL);
-		params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-		tabHost.addView(container, params);
+        LinearLayout container = new LinearLayout(context);
+        container.setOrientation(LinearLayout.VERTICAL);
+        params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        tabHost.addView(container, params);
 
-		TabWidget tabWidget = new TabWidget(context);
-		tabWidget.setId(android.R.id.tabs);
-		tabWidget.setOrientation(LinearLayout.HORIZONTAL);
-		params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-		container.addView(tabWidget, params);
+        FrameLayout tabcontent = new FrameLayout(context);
+        tabcontent.setId(android.R.id.tabcontent);
+        params = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT,1);
+        container.addView(tabcontent, params);
 
-		FrameLayout tabcontent = new FrameLayout(context);
-		tabcontent.setId(android.R.id.tabcontent);
-		params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-		container.addView(tabcontent, params);
+        TabWidget tabWidget = new TabWidget(context);
+        tabWidget.setId(android.R.id.tabs);
+        tabWidget.setOrientation(LinearLayout.HORIZONTAL);
+        params = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT,0);
+        container.addView(tabWidget, params);
 
-		tabHost.setup();
 
-		// For view properties (backgroundColor) and methods (animate())
-		// to work correctly we will use the TabHost as the "native" view.
-		setNativeView(tabHost);
-	}
+        tabHost.setup();
 
-	@Override
-	public void addTab(final TabProxy tab) {
-		TabWidget tabWidget = tabHost.getTabWidget();
+        // For view properties (backgroundColor) and methods (animate())
+        // to work correctly we will use the TabHost as the "native" view.
+        setNativeView(tabHost);
+    }
 
-		final int tabIndex = tabHost.getTabWidget().getTabCount();
+    @Override
+    public void addTab(final TabProxy tab) {
+        TabWidget tabWidget = tabHost.getTabWidget();
 
-		TiUITabHostTab tabView = new TiUITabHostTab(tab);
-		tabViews.put(tabView.id, tabView);
+        final int tabIndex = tabHost.getTabWidget().getTabCount();
 
-		TabSpec tabSpec = tabHost.newTabSpec(tabView.id);
-		tabView.setupTabSpec(tabSpec);
-		tabSpec.setContent(this);
-		tabHost.addTab(tabSpec);
+        TiUITabHostTab tabView = new TiUITabHostTab(tab);
+        tabViews.put(tabView.id, tabView);
 
-		tabView.setIndicatorView(tabWidget.getChildTabViewAt(tabIndex));
+        TabSpec tabSpec = tabHost.newTabSpec(tabView.id);
+        tabView.setupTabSpec(tabSpec);
+        tabSpec.setContent(this);
+        tabHost.addTab(tabSpec);
 
-		// TabHost will automatically select the first tab.
-		// We must suppress the tab selection callback when this selection
-		// happens to comply with the abstract tab group contract.
-		// We will only hook up the tab listener after the first tab is added.
-		if (tabIndex == 0) {
-			tabHost.setOnTabChangedListener(this);
-		}
+        tabView.setIndicatorView(tabWidget.getChildTabViewAt(tabIndex));
 
-		tabHost.getTabWidget().getChildTabViewAt(tabIndex).setOnClickListener(new OnClickListener()
-		{
-			@Override
-			public void onClick(View v)
-			{
-				// The default click listener for tab views is responsible for changing the selected tabs.
-				tabHost.setCurrentTab(tabIndex);
+        // TabHost will automatically select the first tab.
+        // We must suppress the tab selection callback when this selection
+        // happens to comply with the abstract tab group contract.
+        // We will only hook up the tab listener after the first tab is added.
+        if (tabIndex == 0) {
+            tabHost.setOnTabChangedListener(this);
+        }
 
-				tab.fireEvent(TiC.EVENT_CLICK, null);
-			}
-		});
-	}
+        tabHost.getTabWidget().getChildTabViewAt(tabIndex).setOnClickListener(new OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+        {
+            // The default click listener for tab views is responsible for changing the selected tabs.
+            tabHost.setCurrentTab(tabIndex);
 
-	@Override
-	public void removeTab(TabProxy tab) {
-		// Not supported.
-		Log.w(TAG, "Tab removal not supported by this group.");
-	}
+            tab.fireEvent(TiC.EVENT_CLICK, null);
+        }
+        });
+    }
 
-	@Override
-	public void selectTab(TabProxy tab) {
-		TiUITabHostTab tabView = (TiUITabHostTab) tab.peekView();
-		tabHost.setCurrentTabByTag(tabView.id);
-	}
+    @Override
+    public void removeTab(TabProxy tab) {
+        // Not supported.
+        Log.w(TAG, "Tab removal not supported by this group.");
+    }
 
-	@Override
-	public TabProxy getSelectedTab() {
-		String id = tabHost.getCurrentTabTag();
-		TiUITabHostTab tabView = tabViews.get(id);
-		return (TabProxy) tabView.getProxy();
-	}
+    @Override
+    public void selectTab(TabProxy tab) {
+        TiUITabHostTab tabView = (TiUITabHostTab) tab.peekView();
+        tabHost.setCurrentTabByTag(tabView.id);
+    }
 
-	@Override
-	public void onTabChanged(String id)
-	{
-		TabGroupProxy tabGroupProxy = ((TabGroupProxy) proxy);
-		TiUITabHostTab tab = tabViews.get(id);
-		tabGroupProxy.onTabSelected((TabProxy) tab.getProxy());
-	}
+    @Override
+    public TabProxy getSelectedTab() {
+        String id = tabHost.getCurrentTabTag();
+        TiUITabHostTab tabView = tabViews.get(id);
+        return (TabProxy) tabView.getProxy();
+    }
 
-	@Override
-	public View createTabContent(String tag) {
-		TiUITabHostTab tabView = tabViews.get(tag);
-		return tabView.getContentView();
-	}
+    @Override
+    public void onTabChanged(String id)
+    {
+        TabGroupProxy tabGroupProxy = ((TabGroupProxy) proxy);
+        TiUITabHostTab tab = tabViews.get(id);
+        tabGroupProxy.onTabSelected((TabProxy) tab.getProxy());
+    }
 
-	@Override
-	public void propertyChanged(String key, Object oldValue, Object newValue, KrollProxy proxy) {
-		if (key.equals(TiC.PROPERTY_TABS_BACKGROUND_COLOR)) {
-			String activeTab = tabHost.getCurrentTabTag();
-			int color = TiConvert.toColor(newValue.toString());
+    @Override
+    public View createTabContent(String tag) {
+        TiUITabHostTab tabView = tabViews.get(tag);
+        return tabView.getContentView();
+    }
 
-			// Update each inactive tab to the new background color.
-			for (Entry<String, TiUITabHostTab> e : tabViews.entrySet()) {
-				if (e.getKey() == activeTab) {
-					continue;
-				}
-				e.getValue().setBackgroundColor(color);
-			}
+    @Override
+    public void propertyChanged(String key, Object oldValue, Object newValue, KrollProxy proxy) {
+        if (key.equals(TiC.PROPERTY_TABS_BACKGROUND_COLOR)) {
+            String activeTab = tabHost.getCurrentTabTag();
+            int color = TiConvert.toColor(newValue.toString());
 
-		} else {
-			super.propertyChanged(key, oldValue, newValue, proxy);
-		}
-	}
+            // Update each inactive tab to the new background color.
+            for (Entry<String, TiUITabHostTab> e : tabViews.entrySet()) {
+                if (e.getKey() == activeTab) {
+                    continue;
+                }
+                e.getValue().setBackgroundColor(color);
+            }
 
-	@Override
-	public void onFocusChange(final View v, boolean hasFocus)
-	{
-		if (hasFocus) {
-			TiMessenger.postOnMain(new Runnable()
-			{
-				public void run()
-				{
-					TiUIHelper.requestSoftInputChange(proxy, v);
-				}
-			});
-		}
-		// Don't fire focus/blur events here because the the events will be fired through event bubbling.
-	}
+        } else {
+            super.propertyChanged(key, oldValue, newValue, proxy);
+        }
+    }
+
+    @Override
+    public void onFocusChange(final View v, boolean hasFocus)
+    {
+        if (hasFocus) {
+            TiMessenger.postOnMain(new Runnable()
+                    {
+                        public void run()
+            {
+                TiUIHelper.requestSoftInputChange(proxy, v);
+            }
+            });
+        }
+        // Don't fire focus/blur events here because the the events will be fired through event bubbling.
+    }
 
 }
