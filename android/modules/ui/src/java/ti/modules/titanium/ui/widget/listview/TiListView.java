b88682f8be1645f0eb9f78b1dea87bbeeadf6dd6
sabonis <sabonis.tw@gmail.com>
2013-03-28 21:54:07 +0800

    TiListView now implements OnScrollListener

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiListView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiListView.java
@@ -38,7 +38,7 @@ import android.widget.TextView;
 import android.widget.AbsListView;
 //import android.widget.AdapterView;
 
-public class TiListView extends TiUIView {
+public class TiListView extends TiUIView implements AbsListView.OnScrollListener{
 
 	private ListView listView;
 	private TiBaseAdapter adapter;
